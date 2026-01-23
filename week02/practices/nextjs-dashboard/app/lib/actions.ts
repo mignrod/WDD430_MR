91d251ca603ee8fa07ce13574cdaba55b50f6715
mignrod <mignrod@gmail.com>
2026-01-23 19:38:57 -0400

    Completed

--- week02/practices/nextjs-dashboard/app/lib/actions.ts
+++ week02/practices/nextjs-dashboard/app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -69,30 +71,6 @@ export async function createInvoice(prevState: State, formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
-// export async function updateInvoice(id: string, formData: FormData) {
-//   const { customerId, amount, status } = UpdateInvoice.parse({
-//     customerId: formData.get('customerId'),
-//     amount: formData.get('amount'),
-//     status: formData.get('status')
-//   });
-//   const amountInCents = amount * 100;
-
-//   try {
-//     await sql`
-//         UPDATE invoices
-//         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-//         WHERE id = ${id}
-//       `;
-//   } catch (error) {
-//     // We'll also log the error to the console for now
-//     console.error(error);
-//     return { message: 'Database Error: Failed to Update Invoice.' };
-//   }
-
-//   revalidatePath('/dashboard/invoices');
-//   redirect('/dashboard/invoices');
-// }
-
 export async function updateInvoice(
   id: string,
   prevState: State,
@@ -135,4 +113,23 @@ export async function deleteInvoice(id: string) {
     WHERE id = ${id}
     `;
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
